7024dfb760e60455549ca88637be6f970e06505e
Njeri <abishaiep@gmail.com>
2025-01-02 12:56:27 -0500

    init

---
+++ src/main/java/com/example/whale_of_a_time/WhaleOfATimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.whale_of_a_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleOfATimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhaleOfATimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whale_of_a_time/WhaleOfATimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whale_of_a_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleOfATimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
